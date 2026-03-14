2abbba8a3016bc4d9625263490b0bf0ee545c634
彡 Dєƒƒiк 彡 <123771670+Nikita2275@users.noreply.github.com>
2026-03-14 11:48:47 +0300

    Add files via upload
    Создайте функцию findElement, которая принимает указатель на массив, его размер 
    и число для поиска. Функция возвращает индекс первого вхождения числа или -1, 
    если число не найдено.

---
+++ #2.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <random>
+
+using namespace std;
+
+int* numbers{ nullptr };
+int N, P;
+
+void Randomizer()
+{
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 100);
+
+    cout << "Enter the number of elements: ";
+    cin >> N;
+
+    numbers = new int[N];
+
+    cout << "\nGenerated " << N << " random numbers:\n";
+    cout << "------------------------\n";
+    for (int i = 0; i < N; ++i) {
+        numbers[i] = dis(gen);
+        cout << "numbers[" << i << "] = " << numbers[i] << endl;
+    }
+    cout << "------------------------\n\n";
+}
+
+void Poisk() {
+    bool found = false;
+
+    cout << "Enter the number to search for: ";
+    cin >> P;
+
+    cout << "\nSearching for " << P << "...\n";
+    cout << "------------------------\n";
+
+    for (int i = 0; i < N; ++i) {
+        if (numbers[i] == P) {
+            cout << "Found at position: " << i << endl;
+            found = true;
+        }
+    }
+
+    if (!found) {
+        cout << "Number " << P << " not found in the array" << endl;
+    }
+    cout << "------------------------\n\n";
+
+    delete[] numbers;
+    numbers = nullptr;
+}
+
+int main()
+{
+    cout << "=== Random Number Search Program ===\n\n";
+    Randomizer();
+    Poisk();
+    cout << "Program finished.\n";
+
+    return 1;
+}
