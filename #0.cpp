2833f02c78c00a94e1bee9a88136e6686b678e43
彡 Dєƒƒiк 彡 <123771670+Nikita2275@users.noreply.github.com>
2026-03-14 12:15:35 +0300

    Add files via upload
    1. Создайте обычную (стековую) переменную int a = 10 
    2. Создайте указатель ptr, который хранит адрес переменной a 
    3. Выведите на экран: 
    o Значение переменной a 
    o Адрес переменной a 
    o Значение указателя ptr (это тоже адрес) 
    o Значение, на которое указывает ptr (используя разыменование) 
    4. Измените значение a через указатель (*ptr = 20) 
    5. Снова выведите значение a и значение по указателю 
    6. Теперь выделите динамическую память для одного целого числа: 
    o Создайте указатель dynamicPtr и выделите память через new 
    o Запишите в эту память число 42 
    o Выведите значение и адрес динамической переменной 
    o Объясните разницу между адресом указателя и адресом, который хранится в 
    указателе 
    o ВАЖНО: Не забудьте освободить память!

---
+++ #0.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "=== Pointer Basics Demo ===\n\n";
+
+    // Часть 1: Стековая переменная и указатель
+    cout << "1. Stack variable and pointer:\n";
+    cout << "------------------------\n";
+
+    int a = 10;
+    auto* ptr(&a);
+
+    cout << "Value of a: " << a << endl;
+    cout << "Address of a: " << &a << endl;
+    cout << "Value of ptr (address stored): " << ptr << endl;
+    cout << "Value pointed by ptr: " << *ptr << endl;
+    cout << "Address of ptr itself: " << &ptr << endl;
+    cout << "------------------------\n\n";
+
+    // Часть 2: Изменение значения через указатель
+    cout << "2. Changing value through pointer:\n";
+    cout << "------------------------\n";
+
+    *ptr = 20;
+    cout << "After *ptr = 20:\n";
+    cout << "Value of a: " << a << endl;
+    cout << "Value pointed by ptr: " << *ptr << endl;
+    cout << "------------------------\n\n";
+
+    // Часть 3: Динамическая память
+    cout << "3. Dynamic memory allocation:\n";
+    cout << "------------------------\n";
+
+    int* dynamicPtr{ new int{42} };
+
+    cout << "Dynamic variable value: " << *dynamicPtr << endl;
+    cout << "Address stored in dynamicPtr: " << dynamicPtr << endl;
+    cout << "Address of dynamicPtr itself: " << &dynamicPtr << endl;
+
+    delete dynamicPtr;
+    dynamicPtr = nullptr;
+
+    cout << "------------------------\n";
+    cout << "Program finished.\n";
+
+    return 1;
+}
