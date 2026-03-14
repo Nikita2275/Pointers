930d8b20d35fdd4cbed21712ad7cd000623f7f13
彡 Dєƒƒiк 彡 <123771670+Nikita2275@users.noreply.github.com>
2026-03-14 12:42:35 +0300

    Add files via upload
    1. Запрашивает у пользователя размер массива 
    2. Создает динамический массив целых чисел 
    3. Заполняет его числами от 1 до N 
    4. Выводит массив на экран 
    5. Удаляет массив

---
+++ #1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+    cout << "=== Random Number Generator ===\n\n";
+
+    cout << "Enter the number of elements: ";
+    int N;
+    cin >> N;
+
+    int* numbers{ nullptr };
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 100);
+
+    numbers = { new int[N] };
+
+    cout << "\nGenerated " << N << " random numbers:\n";
+    cout << "------------------------\n";
+    for (int i = 0; i < N; ++i) {
+        numbers[i] = dis(gen);
+        cout << "numbers[" << i << "] = " << numbers[i] << endl;
+    }
+    cout << "------------------------\n";
+
+    delete[] numbers;
+    numbers = nullptr;
+
+    cout << "Program finished.\n";
+
+    return 1;
+}
