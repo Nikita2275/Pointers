7bdf0d66031ca27767753c75416bade696e3b75c
彡 Dєƒƒiк 彡 <123771670+Nikita2275@users.noreply.github.com>
2026-03-17 19:22:32 +0300

    Add files via upload
    1. Начинает с массива размера 3 
    2. Заполняет его числами, введенными пользователем 
    3. Если пользователь ввел больше чисел, чем размер массива: 
    o Создать новый массив большего размера (увеличить на 2 элемента) 
    o Скопировать старые данные 
    o Удалить старый массив 
    o Продолжить ввод 
    4. Ввод прекращается, когда пользователь вводит 0

---
+++ #3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "=== Dynamic Array with Auto-Expansion ===\n\n";
+
+	int width = 3;
+	int input;
+	int size = 0;
+	int* numbers{ new int[width] };
+
+	cout << "Enter numbers (0 to stop):\n";
+	cout << "------------------------\n";
+
+	while (cin >> input) {
+		if (input == 0) {
+
+			cout << "\n------------------------";
+			cout << "\nYou entered " << size << " numbers:\n";
+			cout << "------------------------\n";
+
+			for (int i = 0; i < size; ++i) {
+				cout << "numbers[" << i << "] = " << numbers[i] << endl;
+			}
+			
+			cout << "------------------------\n";
+			delete[] numbers;
+			numbers = nullptr;
+			break;
+		}
+		if (size >= width) {
+			cout << "Array expanded: " << width << " -> " << width + 2 << " elements\n";
+			width += 2;
+			int* new_numbers{ new int[width] };
+			for (int i = 0; i < size; ++i) {
+				new_numbers[i] = numbers[i];
+			}
+			delete[] numbers;
+			numbers = new_numbers;
+		}
+		
+		numbers[size] = input;
+		size++;
+	}
+}
