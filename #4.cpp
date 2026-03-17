84c5a17fd4912609d0778b078eea4fdd30a661e9
彡 Dєƒƒiк 彡 <123771670+Nikita2275@users.noreply.github.com>
2026-03-17 20:59:06 +0300

    Add files via upload
    Создайте функцию filterEven, которая принимает массив и его размер, и 
    возвращает НОВЫЙ динамический массив, содержащий только четные числа из 
    исходного. Размер возвращаемого массива должен быть точным (без лишних 
    элементов). 
    Также напишите функцию сортировки (пузырьком или вставками). 
    В main: 
    1. Создать динамический массив случайных чисел 
    2. Вывести исходный массив 
    3. Отфильтровать четные числа 
    4. Отсортировать полученный массив по возрастанию 
    5. Вывести результат 
    Важно: Все функции должны корректно работать с памятью (никаких утечек).

---
+++ #4.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <random>
+using namespace std;
+
+int input;
+int* numbers{ nullptr };
+
+void Randomizer() {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 100);
+
+    numbers = { new int[input] };
+
+    cout << "\nGenerated " << input << " random numbers:\n";
+    cout << "------------------------\n";
+
+    for (int i = 0; i < input; ++i) {
+        numbers[i] = dis(gen);
+        cout << "numbers[" << i << "] = " << numbers[i] << endl;  
+    }
+    cout << "------------------------\n";
+}
+
+void filterEven() {
+
+    int Count = 0;
+    for (int i = 0; i < input; ++i) {
+        if (numbers[i] % 2 == 0) {
+            Count++;
+        }
+    }
+
+    int* numb{ new int[Count] };
+    int inp = 0;
+    for (int i = 0; i < input; ++i) {
+        if (numbers[i] % 2 == 0) {
+            numb[inp] = numbers[i];
+            inp++;
+        }
+    }
+
+    cout << "\nFiltered even numbers (" << Count << " found):\n";
+    cout << "------------------------\n";
+
+    for (int j = 0; j < Count; ++j) {
+        cout << "numbers[" << j << "] = " << numb[j] << endl;
+    }
+    cout << "------------------------\n";
+
+    delete[] numbers;
+    numbers = nullptr;
+    delete[] numb;
+    numb = nullptr;
+}
+
+int main()
+{
+    cout << "=== Even Number Filter ===\n\n";
+    cout << "Enter the number of elements: ";
+
+    cin >> input;
+    Randomizer();
+    filterEven();
+}
